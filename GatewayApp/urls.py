5747fba003be9feca96dd770053638bc21aa7dfb
Gordiig <gordiig@gmail.com>
2020-04-16 19:25:54 +0300

    Add urls

--- GatewayApp/urls.py
+++ GatewayApp/urls.py
@@ -3,5 +3,9 @@ from GatewayApp import views
 
 
 urlpatterns = [
-
+    url(r'^gateway/add_place/$', views.AddPlaceView.as_view()),
+    url(r'^gateway/add_rating/$', views.AddRatingView.as_view()),
+    url(r'^gateway/add_acceptance/$', views.AddAcceptView.as_view()),
+    url(r'^gateway/delete_acceptance/(?P<acceptance_id>\d+)/$', views.DeleteAcceptanceView.as_view()),
+    url(r'^gateway/buy_pin/$', views.BuyPinView.as_view()),
 ]
